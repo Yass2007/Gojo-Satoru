eb60095820e441c878b5d5b19e70fa8770eb986e
Yass2007 <109111344+Yass2007@users.noreply.github.com>
2022-07-13 12:39:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['212675012435']
+global.premium = ['212675012435']
+global.ownernomer = '212675012435'
+global.ownername = '🐦Star Boy Yasser'
+global.botname = 'Yasser-Nigga'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Yasser-Nigga'
+global.author = '🐦Star Boy Yasser'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
